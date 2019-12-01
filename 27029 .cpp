c22c3ca695b318a98be77d611a4656f3c4dd01d7
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2019-12-01 16:49:37 +0800

    Update 27029 .cpp

--- 27029 .cpp
+++ 27029 .cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     long long number;
     do {
         cout << "輸入國際標準書號：";
@@ -12,10 +11,14 @@ int main()
     int lastNumber = number % 10;
     int times = 0;
 
+    number -= lastNumber;
     number /= 10;
 
-    for (int i = 10; i > 1; i --){
-        times += number % 10/i;
+    for (int i = 9; i > 0; i --){
+        int variable = number % 10;
+        times += i * variable;
+
+        number -= number % 10;
         number /= 10;
         
         while (times > 11){
