6f9b2fe74cd79fffac52b845088ba2478962d0bd
firedragon0314 <56679739+firedragon0314@users.noreply.github.com>
2019-12-05 18:58:13 +0800

    Update 27029 .cpp

--- 27029 .cpp
+++ 27029 .cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 int main(){
     long long number;
-    do {
-        cout << "輸入國際標準書號：";
-        cin >> number;
-    } while (number < 1000000000 || number > 10000000000);
-
+    
+    cout << "輸入國際標準書號：";
+    cin >> number;
+    
     int lastNumber = number % 10;
     int times = 0;
 
